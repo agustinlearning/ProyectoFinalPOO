f975296426ddeb8c33ccdd2beed909aa5e0c4a1c
agustin <agustinalvarez7378@gmail.com>
2026-07-14 15:47:11 -0400

    Creando Clase Contraladora Bolsa

---
+++ ProyectoFinal/src/logico/Bolsa.java
@@ -0,0 +1,11 @@
+package logico;
+
+import java.util.ArrayList;
+
+public class Bolsa {
+	public ArrayList<Empresa> lasEmrpesas;
+	public ArrayList<Persona> lasPersonas;
+	public ArrayList<Solicitud> lasSolicitudes;
+	public ArrayList<Oferta> lasOfertas;
+	
+}
