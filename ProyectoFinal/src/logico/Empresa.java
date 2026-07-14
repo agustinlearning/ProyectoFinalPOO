4e9f3e2d318479130f902934457f6981af7d37b1
agustin <agustinalvarez7378@gmail.com>
2026-07-14 14:59:40 -0400

    Creando clase representante y añadiendo atributos.

--- ProyectoFinal/src/logico/Empresa.java
+++ ProyectoFinal/src/logico/Empresa.java
@@ -1,5 +1,56 @@
 package logico;
 
 public class Empresa {
-	private String codigo;
+
+	private String id;
+	private static int counterId=1;
+	private String rnc;
+	private Representante representante;
+	private String razonSocial;
+	private float kpi;
+	private String ubicacion;
+	
+	public Empresa(String rnc, Representante representante, String razonSocial, float kpi, String ubicacion) {
+		super();
+		this.id = ""+counterId++;
+		this.rnc = rnc;
+		this.representante = representante;
+		this.razonSocial = razonSocial;
+		this.kpi = kpi;
+		this.ubicacion = ubicacion;
+	}
+	
+	public String getRnc() {
+		return rnc;
+	}
+	public void setRnc(String rnc) {
+		this.rnc = rnc;
+	}
+	public Representante getRepresentante() {
+		return representante;
+	}
+	public void setRepresentante(Representante representante) {
+		this.representante = representante;
+	}
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	public float getKpi() {
+		return kpi;
+	}
+	public void setKpi(float kpi) {
+		this.kpi = kpi;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public String getId() {
+		return id;
+	}
 }
---
+++ ProyectoFinal/src/logico/Representante.java
@@ -0,0 +1,40 @@
+package logico;
+
+public class Representante {
+
+	private String id;
+	private static int counterId=1;
+	private String cedula;
+	private String nombre;
+	private String cargo;
+	
+	public Representante(String cedula, String nombre, String cargo) {
+		super();
+		this.id = ""+counterId++;
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.cargo = cargo;
+	}
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public String getId() {
+		return id;
+	}
+}
