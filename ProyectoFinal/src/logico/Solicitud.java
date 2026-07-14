3fc4015b91f033908353c81384aaf5545f521a0c
agustin <agustinalvarez7378@gmail.com>
2026-07-14 15:41:52 -0400

    Creando clase Solicitud

---
+++ ProyectoFinal/src/logico/Solicitud.java
@@ -0,0 +1,61 @@
+package logico;
+
+import java.time.LocalDate;
+
+public class Solicitud {
+	
+	private String id;
+	private static int counterId=1;
+	private Persona candidato;
+	private Oferta oferta;
+	private LocalDate fecha;
+	private float porCoincidencia;
+	private String estado;
+	
+	
+	public Solicitud(Persona candidato, Oferta oferta, LocalDate fecha, float porCoincidencia, String estado) {
+		super();
+		this.id = ""+counterId++;
+		this.candidato = candidato;
+		this.oferta = oferta;
+		this.fecha = fecha;
+		this.porCoincidencia = porCoincidencia;
+		this.estado = estado;
+	}
+
+	public Persona getCandidato() {
+		return candidato;
+	}
+	public void setCandidato(Persona candidato) {
+		this.candidato = candidato;
+	}
+	public Oferta getOferta() {
+		return oferta;
+	}
+	public void setOferta(Oferta oferta) {
+		this.oferta = oferta;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	public float getPorCoincidencia() {
+		return porCoincidencia;
+	}
+	public void setPorCoincidencia(float porCoincidencia) {
+		this.porCoincidencia = porCoincidencia;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getId() {
+		return id;
+	}
+	
+	
+}
