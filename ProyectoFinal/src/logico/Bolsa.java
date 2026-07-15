1a39dee5d1b1b326f0d9204596a32540801933f4
agustin <agustinalvarez7378@gmail.com>
2026-07-15 19:55:49 -0400

    añadiendo patron singleton y llamando a bolsa en el main

--- ProyectoFinal/src/logico/Bolsa.java
+++ ProyectoFinal/src/logico/Bolsa.java
@@ -3,11 +3,28 @@ package logico;
 import java.util.ArrayList;
 
 public class Bolsa {
-	public ArrayList<Empresa> lasEmrpesas;
+	private static Bolsa bolsa;
+
+	public ArrayList<Empresa> lasEmpresas;
 	public ArrayList<Persona> lasPersonas;
 	public ArrayList<Solicitud> lasSolicitudes;
 	public ArrayList<Oferta> lasOfertas;
 	
+	private Bolsa() {
+		super();
+		this.lasEmpresas = new ArrayList<>();
+		this.lasPersonas = new ArrayList<>();
+		this.lasSolicitudes = new ArrayList<>();
+		this.lasOfertas = new ArrayList<>();
+	}
+
+	
+	public static Bolsa getBolsa() {
+		if(bolsa == null) {
+			bolsa = new Bolsa();
+		}
+		return bolsa;
+	}
 	
 	public ArrayList<Persona> conectarCandidatos(Oferta oferta){
 		return null;
--- ProyectoFinal/src/logico/Main.java
+++ ProyectoFinal/src/logico/Main.java
@@ -3,7 +3,9 @@ package logico;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Bolsa bolsa = Bolsa.getBolsa();
+		// la linea de abajo es para que no de warning
+		System.out.println(bolsa.lasEmpresas.get(0));
 
 	}
 
