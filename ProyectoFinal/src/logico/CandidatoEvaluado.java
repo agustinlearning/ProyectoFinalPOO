74d6f8425cf58f5de25211d026ae8e22879126fb
juanc <juanc@DESKTOP-C581ORR>
2026-07-19 09:54:00 -0400

    Logica de la clase CandidatoEvaluado

--- ProyectoFinal/src/logico/CandidatoEvaluado.java
+++ ProyectoFinal/src/logico/CandidatoEvaluado.java
@@ -1,5 +1,25 @@
 package logico;
 
 public class CandidatoEvaluado {
+	private Persona candidato;
+	private int puntajeTotal;
 	
+	public CandidatoEvaluado(Persona candidato, int puntajeTotal) {
+		super();
+		this.candidato = candidato;
+		this.puntajeTotal = puntajeTotal;
+	}
+	
+	public Persona getCandidato() {
+		return candidato;
+	}
+	public void setCandidato(Persona candidato) {
+		this.candidato = candidato;
+	}
+	public int getPuntajeTotal() {
+		return puntajeTotal;
+	}
+	public void setPuntajeTotal(int puntajeTotal) {
+		this.puntajeTotal = puntajeTotal;
+	}
 }
