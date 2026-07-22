0ac39d9cb21e026a84ff5378c2ddf7ba6ee50d1a
juanc <juanc@DESKTOP-C581ORR>
2026-07-22 14:08:41 -0400

    Modificacion de la ventana principal

--- ProyectoFinal/src/visual/Principal.java
+++ ProyectoFinal/src/visual/Principal.java
@@ -41,43 +41,6 @@ public class Principal extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 834, 529);
 		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu MenuVacas = new JMenu("Vacas");
-		menuBar.add(MenuVacas);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Agregar");
-		mntmNewMenuItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		MenuVacas.add(mntmNewMenuItem);
-		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Listar");
-		mntmNewMenuItem_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		MenuVacas.add(mntmNewMenuItem_1);
-		
-		JMenu mnNewMenu = new JMenu("Toros");
-		menuBar.add(mnNewMenu);
-		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("New menu item");
-		mnNewMenu.add(mntmNewMenuItem_2);
-		
-		JMenuItem mntmNewMenuItem_4 = new JMenuItem("New menu item");
-		mnNewMenu.add(mntmNewMenuItem_4);
-		
-		JMenu mnNewMenu_1 = new JMenu("Becerritos");
-		menuBar.add(mnNewMenu_1);
-		
-		JMenuItem mntmNewMenuItem_3 = new JMenuItem("New menu item");
-		mnNewMenu_1.add(mntmNewMenuItem_3);
-		
-		JMenuItem mntmNewMenuItem_5 = new JMenuItem("New menu item");
-		mnNewMenu_1.add(mntmNewMenuItem_5);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
