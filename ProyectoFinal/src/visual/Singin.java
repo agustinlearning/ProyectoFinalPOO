ee33be4335aadf5c2dfb4db76b698d019a00491d
juanc <juanc@DESKTOP-C581ORR>
2026-07-22 14:09:15 -0400

    Prototipo de ventana Singin

---
+++ ProyectoFinal/src/visual/Singin.java
@@ -0,0 +1,121 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+public class Singin extends JFrame {
+
+	private JPanel contentPane;
+	private JFormattedTextField txtPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Singin frame = new Singin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Singin() {
+		setTitle("Sing in");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 640, 400);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Correo Electronico");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblNewLabel.setBounds(77, 38, 169, 38);
+		panel.add(lblNewLabel);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
+		lblContrasea.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblContrasea.setBounds(77, 114, 169, 38);
+		panel.add(lblContrasea);
+		
+		txtPassword = new JFormattedTextField();
+		txtPassword.setBounds(272, 120, 257, 26);
+		panel.add(txtPassword);
+		
+		JLabel lblNewLabel_1 = new JLabel("\u00BFTienes una cuenta?");
+		lblNewLabel_1.setBounds(52, 266, 223, 20);
+		panel.add(lblNewLabel_1);
+		
+		Button button = new Button("Inicia sesion");
+		button.setActionCommand("");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		button.setBounds(272, 259, 128, 27);
+		panel.add(button);
+		
+		JFormattedTextField txtEmail = new JFormattedTextField();
+		txtEmail.setBounds(272, 44, 257, 26);
+		panel.add(txtEmail);
+		
+		JLabel lblTipo = new JLabel("Tipo");
+		lblTipo.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lblTipo.setBounds(77, 190, 169, 38);
+		panel.add(lblTipo);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Empresa", "Persona"}));
+		comboBox.setBounds(272, 196, 128, 26);
+		panel.add(comboBox);
+		
+		Panel panel_1 = new Panel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JToolBar toolBar = new JToolBar();
+		panel_1.add(toolBar, BorderLayout.EAST);
+		
+		JButton btnNewButton = new JButton("Registrar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		toolBar.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Cancelar");
+		toolBar.add(btnNewButton_1);
+	}
+}
