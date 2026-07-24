6e73decd9df1b170f0f90a52890a3a732ec8dd1f
juanc <juanc@DESKTOP-C581ORR>
2026-07-24 14:06:55 -0400

    Maquetado de la ventana principal

--- ProyectoFinal/src/visual/Principal.java
+++ ProyectoFinal/src/visual/Principal.java
@@ -11,10 +11,17 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
+
+import java.awt.Component;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
+	private JMenu mnEmpleados;
+	private JMenu mnOfertas;
+	private JMenu mnPerfil;
 
 	/**
 	 * Launch the application.
@@ -41,6 +48,40 @@ public class Principal extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 834, 529);
 		setVisible(false);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnEmpleados = new JMenu("Solicitudes");
+		mnEmpleados.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		menuBar.add(mnEmpleados);
+		
+		JMenuItem mntmCrearSolicitud = new JMenuItem("Registrar");
+		mntmCrearSolicitud.setHorizontalAlignment(SwingConstants.CENTER);
+		mnEmpleados.add(mntmCrearSolicitud);
+		
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Listar");
+		mnEmpleados.add(mntmNewMenuItem);
+		
+		mnOfertas = new JMenu("Ofertas");
+		menuBar.add(mnOfertas);
+		
+		JMenuItem mntmRegistrarOferta = new JMenuItem("Registrar");
+		mntmRegistrarOferta.setHorizontalAlignment(SwingConstants.LEFT);
+		mnOfertas.add(mntmRegistrarOferta);
+		
+		JMenuItem mntmListarOfertas = new JMenuItem("Listar");
+		mnOfertas.add(mntmListarOfertas);
+		
+		mnPerfil = new JMenu("Perfil");
+		menuBar.add(mnPerfil);
+		
+		JMenuItem mntmMostrarPerfil = new JMenuItem("Ver perfil");
+		mnPerfil.add(mntmMostrarPerfil);
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
