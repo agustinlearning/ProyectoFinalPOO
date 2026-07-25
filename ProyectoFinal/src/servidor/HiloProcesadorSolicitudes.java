46adf7c14030e86fcff267ff62d02fb793603967
agustin <agustinalvarez7378@gmail.com>
2026-07-25 19:03:35 -0400

    segundo hilo, filtrado automatico, actualizasion dinamica del estado

--- ProyectoFinal/src/servidor/HiloProcesadorSolicitudes.java
+++ ProyectoFinal/src/servidor/HiloProcesadorSolicitudes.java
@@ -2,6 +2,12 @@ package servidor;
 
 import logico.Bolsa;
 import logico.Solicitud;
+import logico.Universitario;
+import logico.Tecnico;
+import logico.Obrero;
+import logico.Oferta;
+import logico.Persona;
+
 
 
 public class HiloProcesadorSolicitudes implements Runnable{
@@ -13,10 +19,38 @@ public class HiloProcesadorSolicitudes implements Runnable{
 			try {
 				for(Solicitud sol : Bolsa.getBolsa().lasSolicitudes) {
 					if(sol.getEstado().equalsIgnoreCase("disponible")) {
-						if(sol.getOferta().isNeedLicencia()) {
-							
-						}
+						sol.setEstado("revision");
+						continue;
 					}
+					
+					if(sol.getEstado().equalsIgnoreCase("revision")) {
+						Persona candidato = sol.getCandidato();
+						Oferta ofer = sol.getOferta();
+						boolean rechazo = false;
+						
+						if (ofer.isNeedLicencia() && !candidato.isLicencia()) {
+                            rechazo = true;
+                        }
+						
+						String tipo = ofer.getTipoCandidato();
+						if(tipo.equalsIgnoreCase("Universitario") && !(candidato instanceof Universitario)) {
+							rechazo = true;
+						}
+						if(tipo.equalsIgnoreCase("Tecnico") && !(candidato instanceof Tecnico)) {
+							rechazo = true;
+						}
+						if(tipo.equalsIgnoreCase("obrero") && !(candidato instanceof Obrero)) {
+							rechazo = true;
+						}
+						
+						if (rechazo) {
+                            sol.setEstado("Rechazada/o");
+                        } else {
+                            sol.setEstado("Aprobada/o para Entrevista");
+                        }
+					}
+					
+					
 				}
 				Thread.sleep(600000);
 			} catch(InterruptedException e){
