9777fe624c8319bf3ba479f20cb865f685b948a3
juanc <juanc@DESKTOP-C581ORR>
2026-07-28 18:49:42 -0400

    Maquetado de Crear el perfil de la empresa

--- ProyectoFinal/src/visual/RegEmpresa.java
+++ ProyectoFinal/src/visual/RegEmpresa.java
@@ -7,10 +7,20 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RegEmpresa extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -29,17 +39,57 @@ public class RegEmpresa extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegEmpresa() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Crear Perfil");
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				
+			}
+		});
+		setBounds(100, 100, 640, 262);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Rnc");
+		lblNewLabel.setBounds(50, 45, 69, 20);
+		contentPanel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Razon social");
+		lblNewLabel_1.setBounds(309, 45, 101, 20);
+		contentPanel.add(lblNewLabel_1);
+		{
+			JLabel lblUbicacion = new JLabel("Ubicaci\u00F3n");
+			lblUbicacion.setBounds(50, 115, 75, 20);
+			contentPanel.add(lblUbicacion);
+		}
+		
+		textField = new JTextField();
+		textField.setBounds(138, 42, 146, 26);
+		contentPanel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(138, 112, 146, 26);
+		contentPanel.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(425, 42, 146, 26);
+		contentPanel.add(textField_2);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
@@ -51,5 +101,4 @@ public class RegEmpresa extends JDialog {
 			}
 		}
 	}
-
 }
