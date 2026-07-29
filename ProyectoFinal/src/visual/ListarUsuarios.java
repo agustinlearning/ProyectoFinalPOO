21430ac68ea2302df8bb272f34ea34d54c46810e
juanc <juanc@DESKTOP-C581ORR>
2026-07-29 12:41:11 -0400

    Ventana de listar usuarios

---
+++ ProyectoFinal/src/visual/ListarUsuarios.java
@@ -0,0 +1,136 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+
+import logico.Bolsa;
+import logico.Usuario;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.border.TitledBorder;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ListarUsuarios extends JDialog {
+
+	private JPanel contentPane = new JPanel();
+	private DefaultTableModel model;
+	private JTable table;
+	private Object[] row;
+	private Usuario selected = null;
+	private JButton btnModificar;
+	private JButton btnEliminar;
+	private JButton btnCalcularTotal;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListarUsuarios dialog = new ListarUsuarios();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ListarUsuarios() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 630, 319);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		String[] headers = {"Id","Username","Cedula","Email"};
+		model = new DefaultTableModel();
+		model.setColumnIdentifiers(headers);
+		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int index = table.getSelectedRow();
+				if(index >= 0)
+				{
+					btnEliminar.setEnabled(true);
+					btnModificar.setEnabled(true);
+					selected = Bolsa.getBolsa().buscarUsuarioPorUsername(table.getValueAt(index, 0).toString());
+				}
+			}
+		});
+		table.setModel(model);
+		scrollPane.setViewportView(table);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(panel, BorderLayout.SOUTH);
+		panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		
+		btnCalcularTotal.setEnabled(false);
+		btnCalcularTotal.setActionCommand("OK");
+		panel.add(btnCalcularTotal);
+		btnModificar.setActionCommand("OK");
+		panel.add(btnModificar);
+		
+		btnEliminar.setEnabled(false);
+		btnEliminar.setActionCommand("OK");
+		panel.add(btnEliminar);
+		
+		JButton button_1 = new JButton("Cancelar");
+		button_1.setActionCommand("Cancel");
+		panel.add(button_1);
+		
+		
+		loadAlquileres();
+	}
+
+	private void loadAlquileres() {
+		model.setRowCount(0);
+		row = new Object[model.getColumnCount()];
+		for(Usuario user : Bolsa.getBolsa().losUsuarios)
+		{
+			row[0] = user.getId();
+			row[1] = user.getUsername();
+			if(user.getPerfilP()!= null)
+			{
+				row[2] = user.getPerfilP().getCedula();
+			}
+			else
+			{
+				row[2] = "No tiene perfil";
+			}
+			
+			row[3] = user.getEmail();
+			model.addRow(row);
+		}
+		
+	}
+
+}
