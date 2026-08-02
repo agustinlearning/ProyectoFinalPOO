0fdc4f7b0ea4fc97667d6eebfc38ebfdf40d2b8a
juanc <juanc@DESKTOP-C581ORR>
2026-08-02 19:50:37 -0400

    Añadido grafico

--- ProyectoFinal/src/visual/Dashboard.java
+++ ProyectoFinal/src/visual/Dashboard.java
@@ -6,43 +6,47 @@ import java.awt.GridLayout;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import logico.Bolsa;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.border.TitledBorder;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
 
 public class Dashboard extends JDialog {
 
     private final JPanel contentPanel = new JPanel();
-
     public Dashboard() {
         setModal(true);
         setTitle("Resumen del Sistema");
-        setBounds(100, 100, 500, 300);
+        setBounds(100, 100, 723, 477);
         setLocationRelativeTo(null);
         getContentPane().setLayout(new BorderLayout());
-        contentPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+        contentPanel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
-        contentPanel.setLayout(new GridLayout(2, 2, 15, 15));
-
-        contentPanel.add(crearPanelMetrica("Total de Usuarios", Bolsa.getBolsa().losUsuarios.size()));
-        contentPanel.add(crearPanelMetrica("Total de Empresas", Bolsa.getBolsa().lasEmpresas.size()));
-        contentPanel.add(crearPanelMetrica("Personas Registradas", Bolsa.getBolsa().lasPersonas.size()));
-        
-        //recordar añadir otra estadística: ej (la lista de ofertas o solicitudes) mas tarde
-        contentPanel.add(crearPanelMetrica("Ofertas Activas", 0)); 
-    }
-
-    private JPanel crearPanelMetrica(String titulo, int valor) {
-        JPanel panel = new JPanel();
-        panel.setBorder(new TitledBorder(null, titulo, TitledBorder.LEADING, TitledBorder.TOP, new Font("Segoe UI", Font.BOLD, 12), null));
-        panel.setLayout(new BorderLayout(0, 0));
-        
-        JLabel lblValor = new JLabel(String.valueOf(valor));
-        lblValor.setHorizontalAlignment(SwingConstants.CENTER);
-        lblValor.setFont(new Font("Segoe UI", Font.BOLD, 24));
-        panel.add(lblValor, BorderLayout.CENTER);
-        
-        return panel;
+        contentPanel.setLayout(null);
+        getContentPane().add(contentPanel);
+        // Fuente de Datos
+        DefaultPieDataset data = new DefaultPieDataset();
+        data.setValue("Total de Usuarios", Bolsa.getBolsa().losUsuarios.size());
+        data.setValue("Total de Empresas", Bolsa.getBolsa().lasEmpresas.size());
+        data.setValue("Personas Registradas", Bolsa.getBolsa().lasPersonas.size());
+ 
+        // Creando el Grafico
+        JFreeChart chart = ChartFactory.createPieChart(
+         "Ejemplo Rapido de Grafico en un ChartFrame", 
+         data, 
+         true, 
+         true, 
+         false);
+ 
+        // Crear el Panel del Grafico con ChartPanel
+        ChartPanel chartPanel = new ChartPanel(chart);
+        contentPanel.add(chartPanel);
     }
 }
