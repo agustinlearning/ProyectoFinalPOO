968aa9b6237be7ab65ed70c6f8e18dc815ab2a98
agustin <agustinalvarez7378@gmail.com>
2026-08-02 14:37:57 -0400

    cambiando logica del cliente para conectarse con el servidor y probar

--- ProyectoFinal/src/servidor/Cliente.java
+++ ProyectoFinal/src/servidor/Cliente.java
@@ -1,13 +1,18 @@
 package servidor;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Cliente {
-	public static void enviarRespaldoAlServidor() {
+    
+    // Ahora recibe la IP y el puerto por parámetro
+    public static void enviarRespaldoAlServidor(String ip, int puerto) {
         
         File archivoLocal = new File("bolsa_datos.dat"); 
 
@@ -16,18 +21,25 @@ public class Cliente {
             return;
         }
 
-        System.out.println("Intentando conectar con el servidor...");
+        System.out.println("Intentando conectar con el servidor en " + ip + ":" + puerto);
 
-        try (Socket socket = new Socket("localhost", 7000)) {
+        try (Socket socket = new Socket(ip, puerto)) {
             System.out.println("Conexion establecida");
             
             FileInputStream lectorArchivo = new FileInputStream(archivoLocal);
-            OutputStream salidaDatos = socket.getOutputStream();
+            DataOutputStream salidaDatos = new DataOutputStream(socket.getOutputStream());
+            
+            LocalDateTime ahora = LocalDateTime.now();
+            DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            String fechaFormateada = ahora.format(formateador);
+            
+            salidaDatos.writeUTF(fechaFormateada);
+            System.out.println("Fecha enviada: " + fechaFormateada);
             
             byte[] buffer = new byte[4096];
             int bytesLeidos;
             
-            System.out.println("Enviando archivo");
+            System.out.println("Enviando archivo...");
             
             while ((bytesLeidos = lectorArchivo.read(buffer)) != -1) {
                 salidaDatos.write(buffer, 0, bytesLeidos);
@@ -35,6 +47,7 @@ public class Cliente {
             
             salidaDatos.flush();
             lectorArchivo.close();
+            System.out.println("Respaldo enviado exitosamente.");
 
         } catch (IOException e) {
             System.out.println("No se pudo conectar al servidor: " + e.getMessage());
