0ea676a3803f1af7c1a5488f9066d99276aa53cb
juanc <juanc@DESKTOP-C581ORR>
2026-08-03 13:53:10 -0400

    Añadidos label del empleado mejor pagado y empresa con mas empleados

--- ProyectoFinal/src/visual/Dashboard.java
+++ ProyectoFinal/src/visual/Dashboard.java
@@ -10,6 +10,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import logico.Bolsa;
+import logico.Empresa;
+import logico.Persona;
+
 import java.awt.FlowLayout;
 import javax.swing.BoxLayout;
 import javax.swing.border.TitledBorder;
@@ -21,9 +24,11 @@ import org.jfree.data.general.DefaultPieDataset;
 
 public class Dashboard extends JDialog {        
         JPanel panel;
+        private JLabel label;
+        private JLabel lblNewLabel;
         public Dashboard(){
             setTitle("Dashboard");
-            setSize(800,600);
+            setSize(800,398);
             setLocationRelativeTo(null);
             setVisible(true);
             init();
@@ -52,13 +57,41 @@ public class Dashboard extends JDialog {
             chartPanel.setBounds(15, 16, 525, 313);
             panel.add(chartPanel);
             
-            JLabel lblNewLabel = new JLabel("Empleado mejor pagado");
-            lblNewLabel.setBounds(555, 16, 208, 20);
+            lblNewLabel = new JLabel("Empleado mejor pagado");
+            lblNewLabel.setBounds(555, 79, 208, 20);
             panel.add(lblNewLabel);
             
-            JLabel label = new JLabel("Empleado mejor pagado");
-            label.setBounds(555, 119, 208, 20);
+            label = new JLabel("Empleado mejor pagado");
+            Persona empleado = Bolsa.getBolsa().getEmpleadoMejorPagado();
+            if(empleado != null)
+            {
+            	JLabel lblNewLabel_1 = new JLabel("" + empleado.getNombre());
+                lblNewLabel_1.setBounds(555, 132, 175, 20);
+                panel.add(lblNewLabel_1);
+            }
+            else
+            {
+            	JLabel lblNewLabel_1 = new JLabel("No existe");
+                lblNewLabel_1.setBounds(555, 132, 175, 20);
+                panel.add(lblNewLabel_1);
+            }
+            label.setBounds(555, 197, 208, 20);
             panel.add(label);
+            
+            
+            Empresa empresa = Bolsa.getBolsa().getEmpresaConMasEmpleados();
+            if(empresa != null)
+            {
+            	JLabel label_1 = new JLabel("" + empresa.getRnc());
+            	label_1.setBounds(555, 230, 175, 20);
+                panel.add(label_1);
+            }
+            else
+            {
+            	JLabel label_1 = new JLabel("No existe");
+            	label_1.setBounds(555, 230, 175, 20);
+                panel.add(label_1);
+            }
         }
         
         public static void main(String args[]){
